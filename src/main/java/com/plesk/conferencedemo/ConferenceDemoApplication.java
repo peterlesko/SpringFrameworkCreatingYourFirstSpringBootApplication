373863efb3749fceaa535ae01573897667c4f48d
peterlesko <peterlesko@yahoo.com>
2020-04-11 11:42:33 +0100

    completed_2nd_chapter

--- src/main/java/com/plesk/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/plesk/conferencedemo/ConferenceDemoApplication.java
@@ -9,5 +9,9 @@ public class ConferenceDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoApplication.class, args);
 	}
-
 }
+
+//controllers -for API controllers
+//models - JPA entities
+//repositories -JPA repositories
+//services - service or logic based code that may need to go into application
